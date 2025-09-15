9e19a07ecc90fec3d587ce7e4444660e0f22a43c
Rena Shen <renashen314@hotmail.com>
2025-09-14 20:32:08 -0400

    add price filters for products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,26 +1,42 @@
 import { Suspense } from "react";
-import { ProductList, ProductListIsLoading } from "@/modules/products/ui/components/ProductList";
+import {
+  ProductList,
+  ProductListIsLoading,
+} from "@/modules/products/ui/components/ProductList";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ProductFilters } from "@/modules/products/ui/components/ProductFilters";
 
 interface Props {
   params: Promise<{
     category: string;
   }>;
 }
-const Page = async ({params}: Props) => {
-  const {category} = await params;
+const Page = async ({ params }: Props) => {
+  const { category } = await params;
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-    category
-  }));
+  void queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({
+      category,
+    })
+  );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<ProductListIsLoading />}>
-      <ProductList category={category}/>
-      </Suspense>
+      <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+          <div className="lg:col-span-2 xl:col-span-2">
+            <ProductFilters />
+          </div>
+        <div className="lg:col-span-4 xl:col-span-6">
+
+          <Suspense fallback={<ProductListIsLoading />}>
+            <ProductList category={category} />
+          </Suspense>
+        </div>
+        </div>
+      </div>
     </HydrationBoundary>
   );
 };
