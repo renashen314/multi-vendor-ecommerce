0a71287fc504c4237cac1a7fa8485068397ef36d
Rena Shen <renashen314@hotmail.com>
2025-09-17 11:54:01 -0400

    fix dropdown menu position

--- src/modules/home/ui/components/search-filters/SubcategoryMenu.tsx
+++ src/modules/home/ui/components/search-filters/SubcategoryMenu.tsx
@@ -6,12 +6,10 @@ import Link from "next/link";
 interface SubcategoryMenuProps {
   category: CategoriesGetManyOutput[1]
   isOpen: boolean;
-  position: { top: number; left: number };
 }
 export const SubcategoryMenu = ({
   category,
   isOpen,
-  position,
 }: SubcategoryMenuProps) => {
   if (
     !isOpen ||
@@ -25,10 +23,10 @@ export const SubcategoryMenu = ({
 
   return (
     <div
-      className="fixed z-100"
+      className="absolute z-100"
       style={{
-        top: position.top,
-        left: position.left,
+        top: "100%",
+        left: 0,
       }}
     >
       {/* invisible bridge to maintain hover */}
